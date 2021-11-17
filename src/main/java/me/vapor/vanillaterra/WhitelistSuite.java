2ef746da1a6e761bcef3915275091ea590f91566
VapoR <74574116+vaporrrr@users.noreply.github.com>
2021-11-16 21:52:52 -0500

    Rename

--- src/main/java/me/vapor/vanillaterra/WhitelistSuite.java
+++ src/main/java/me/vapor/vanillaterra/WhitelistSuite.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Objects;
 
-public class Main extends JavaPlugin {
+public class WhitelistSuite extends JavaPlugin {
     @Override
     public void onEnable() {
         this.getLogger().info("VanillaTerra Ready!");
